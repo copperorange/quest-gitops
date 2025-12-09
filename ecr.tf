d43a1d597a6a5b26d7648436e6a50fec6ce55e5b
Chris Bell <chris@copperorange.com>
2025-12-09 00:11:43 -0500

    Change ECR to MUTABLE tags for continuous deployment

--- ecr.tf
+++ ecr.tf
@@ -1,6 +1,6 @@
 resource "aws_ecr_repository" "app_repo" {
   name                 = "quest-app"
-  image_tag_mutability = "IMMUTABLE"
+  image_tag_mutability = "MUTABLE"
   force_delete         = true
 
   image_scanning_configuration {
